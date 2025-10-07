5ba90d974aa8e450e5816d874f7e5f64d1de2c4f
NachoTaborda <itaborda@frro.utn.edu.ar>
2025-10-07 17:31:20 -0300

    Mejora de la pagina Notfound

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,30 @@
 import { Link } from "react-router-dom";
-
 function NotFound() {
   return (
-    <div className="container">
-      <h1>404 - Página no encontrada</h1>
-      <p>Lo sentimos, la página que buscas no existe.</p>
-      <Link to="/">Volver al inicio</Link>
+    <div className="relative min-h-screen">
+      <div
+        className="absolute inset-0 w-full h-full z-0"
+        style={{
+          backgroundImage: "url('./src/assets/verstappen-patada-baku.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          filter: "blur(6px)",
+        }}
+      ></div>
+      <div className="relative z-10 flex flex-col justify-center items-center min-h-screen pt-20 pb-20 px-4">
+        <h1 className="text-20xl md:text-9xl font-bold text-white drop-shadow-2xl mb-4 text-center">
+          404
+        </h1>
+        <p className="text-lg text-white/90 drop-shadow-md mb-8 text-center max-w-md font-bold">
+          Lo sentimos, la página que buscas no existe.
+        </p>
+        <Link
+          to="/"
+          className="bg-white/20 backdrop-blur-sm text-white font-medium px-6 py-3 rounded-lg border border-white/30 hover:bg-white/30 transition-all duration-300"
+        >
+          Volver al inicio
+        </Link>
+      </div>
     </div>
   );
 }
