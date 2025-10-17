f82deb4482fc6041a604a352f0cebcb1bb07c33f
SantiTabordaa <staborda@frro.utn.edu.ar>
2025-10-17 10:36:53 -0300

    Listado de escuderias Creado, queda refinar interfaz, cargo lo hecho

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Pilotos from "./pages/pilotos/Pilotos.tsx";
-import Escuderias from "./pages/escuderias/Escuderias.tsx";
+import ListadoEscuderias from "./pages/escuderias/ListadoEscuderias.tsx";
 import Marcas from "./pages/Marcas.tsx";
 import Circuitos from "./pages/circuitos/Circuitos.tsx";
 import Categorias from "./pages/Categorias.tsx";
@@ -25,7 +25,7 @@ function App() {
         <Route path="calendario" element={<Calendario />} />
         <Route path="*" element={<NotFound />} />
         <Route path="pilotos" element={<Pilotos />} />
-        <Route path="escuderias" element={<Escuderias />} />
+        <Route path="escuderias" element={<ListadoEscuderias />} />
         <Route path="marcas" element={<Marcas />} />
         <Route path="circuitos" element={<Circuitos />} />
         <Route path="categorias" element={<Categorias />} />
--- src/entities/escuderia.entity.ts
+++ src/entities/escuderia.entity.ts
@@ -1,16 +1,13 @@
 import { Piloto } from './piloto.entity.js';
 import { baseEntity } from './baseEntity.entity.js';
 import { Marca } from './marca.entity.js';
-import {Categoria} from './categoria.entity.js';
+import { Categoria } from './categoria.entity.js';
 
-export class Escuderia extends baseEntity{
-
-    pilotos?: Piloto[];
-    fundation!:number
-    nationality!:string
-    engine!:string
-    marca!: Marca
-    categoria!: Categoria
-   
+export class Escuderia extends baseEntity {
+  pilotos?: Piloto[];
+  fundation!: number;
+  nationality!: string;
+  engine!: string;
+  marca!: Marca;
+  categoria!: Categoria;
 }
-
